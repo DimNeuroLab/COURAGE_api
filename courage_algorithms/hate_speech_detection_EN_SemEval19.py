a12478ad3848ebd79004818e8083224fa5514379
Gregor Donabauer <gregor.donabauer@gmx.de>
2022-03-31 14:16:41 +0200

    English Hate Speech model

---
+++ courage_algorithms/hate_speech_detection_EN_SemEval19.py
@@ -0,0 +1,29 @@
+import torch
+import numpy as np
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from setup import get_working_dir
+
+
+def load_tokenizer():
+    return AutoTokenizer.from_pretrained("pysentimiento/bertweet-hate-speech",
+                                         cache_dir=get_working_dir() + '/courage_algorithms/models/Bertweet_hate_speech_EN')
+
+
+def load_model():
+    return AutoModelForSequenceClassification.from_pretrained("pysentimiento/bertweet-hate-speech",
+                                                              cache_dir=get_working_dir() + '/courage_algorithms/models/Bertweet_hate_speech_EN')
+
+
+def predict_hate_speech_en_semeval19(sentence):
+    tokenizer = load_tokenizer()
+    model = load_model()
+
+    inputs = tokenizer(sentence, return_tensors="pt")
+
+    outputs = model(**inputs)
+    logits = outputs[0]
+    logits = logits.squeeze(0)
+    proba = torch.sigmoid(logits)
+    hateful, targeted, aggressive = proba[0], proba[1], proba[2]
+
+    return np.round(hateful.item(), 4), np.round(targeted.item(), 4), np.round(aggressive.item(), 4)
