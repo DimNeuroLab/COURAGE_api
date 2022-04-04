c0b4fd9bb056da3102e270df67adc12387b06766
Gregor Donabauer <gregor.donabauer@gmx.de>
2022-04-04 15:26:38 +0200

    Spanish sentiment analysis

---
+++ courage_algorithms/ES_text_algorithms/sentiment_ES.py
@@ -0,0 +1,30 @@
+import torch
+import numpy as np
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from setup import get_working_dir
+
+
+def load_tokenizer():
+    return AutoTokenizer.from_pretrained("pysentimiento/robertuito-sentiment-analysis",
+                                         cache_dir=get_working_dir() + '/courage_algorithms/models/RoBERTuito_sentiment_ES')
+
+
+def load_model():
+    return AutoModelForSequenceClassification.from_pretrained("pysentimiento/robertuito-sentiment-analysis",
+                                                              cache_dir=get_working_dir() + '/courage_algorithms/models/RoBERTuito_sentiment_ES')
+
+
+def predict_sentiment_es(sentence):
+    tokenizer = load_tokenizer()
+    model = load_model()
+
+    inputs = tokenizer(sentence, return_tensors="pt")
+
+    labels = torch.tensor([1]).unsqueeze(0)
+    outputs = model(**inputs, labels=labels)
+    loss, logits = outputs[:2]
+    logits = logits.squeeze(0)
+    proba = torch.softmax(logits, dim=0)
+    negative, neutral, positive = proba
+
+    return np.round(negative.item(), 4), np.round(neutral.item(), 4), np.round(positive.item(), 4)
