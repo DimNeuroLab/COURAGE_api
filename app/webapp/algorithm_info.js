1c1e67ecb508e4ead2c9a16f7adaf00ab25f14e4
Gregor Donabauer <gregor.donabauer@gmx.de>
2022-06-30 09:40:02 +0200

    added info on algorithm overview page

--- app/webapp/algorithm_info.js
+++ app/webapp/algorithm_info.js
@@ -27,7 +27,13 @@ async function load_algorithm_samples() {
     var s = '<table><tr><th>Sample</th><th>Prediction</th><th>True</th></tr>';
     en_sentiment_strings.push(s);
     for (let prediction of en_sentiment_out) {
-        s = '<tr><td>' + prediction[0] + '</td><td>' + prediction[1] + '</td><td>' + prediction[2] + '</td></tr>';
+        if (prediction[1] == prediction[2]) {
+            s = '<tr><td>' + prediction[0] + '</td><td style="color: green;">' + prediction[1] + '</td><td style="color: green;">' +
+             prediction[2] + '</td></tr>';
+        } else {
+            s = '<tr><td>' + prediction[0] + '</td><td style="color: red;">' + prediction[1] + '</td><td style="color: red;">' +
+             prediction[2] + '</td></tr>';
+        }
         en_sentiment_strings.push(s);
     }
     var s = '</table>';
@@ -44,7 +50,13 @@ async function load_algorithm_samples() {
     var s = '<table><tr><th>Sample</th><th>Prediction</th><th>True</th></tr>';
     en_emotion_strings.push(s);
     for (let prediction of en_emotion_out) {
-        s = '<tr><td>' + prediction[0] + '</td><td>' + prediction[1] + '</td><td>' + prediction[2] + '</td></tr>';
+        if (prediction[1] == prediction[2]) {
+            s = '<tr><td>' + prediction[0] + '</td><td style="color: green;">' + prediction[1] + '</td><td style="color: green;">' +
+             prediction[2] + '</td></tr>';
+        } else {
+            s = '<tr><td>' + prediction[0] + '</td><td style="color: red;">' + prediction[1] + '</td><td style="color: red;">' +
+             prediction[2] + '</td></tr>';
+        }
         en_emotion_strings.push(s);
     }
     var s = '</table>';
@@ -64,9 +76,28 @@ async function load_algorithm_samples() {
     en_hate_speech_strings.push(s);
     var label_dict = {0: 'false', 1: 'true'};
     for (let prediction of en_hate_out) {
-        s = '<tr><td>' + prediction[0] + '</td><td>' + label_dict[prediction[1]] + '</td><td>' + label_dict[prediction[2]] +'</td>' +
-            '<td>' + label_dict[prediction[3]] + '</td><td>' + label_dict[prediction[4]] +'</td>' +
-            '<td>' + label_dict[prediction[5]] + '</td><td>' + label_dict[prediction[6]] +'</td></tr>';
+        s = '<tr><td>' + prediction[0] + '</td>';
+        en_hate_speech_strings.push(s);
+
+        if (label_dict[prediction[1]] == label_dict[prediction[2]]) {
+            s = '<td style="color: green;">' + label_dict[prediction[1]] + '</td><td style="color: green;">' + label_dict[prediction[2]] +'</td>';
+        } else {
+            s = '<td style="color: red;">' + label_dict[prediction[1]] + '</td><td style="color: red;">' + label_dict[prediction[2]] +'</td>';
+        }
+        en_hate_speech_strings.push(s);
+        if (label_dict[prediction[3]] == label_dict[prediction[4]]) {
+            s = '<td style="color: green;">' + label_dict[prediction[3]] + '</td><td style="color: green;">' + label_dict[prediction[4]] +'</td>';
+        } else {
+            s = '<td style="color: red;">' + label_dict[prediction[3]] + '</td><td style="color: red;">' + label_dict[prediction[4]] +'</td>';
+        }
+        en_hate_speech_strings.push(s);
+        if (label_dict[prediction[5]] == label_dict[prediction[6]]) {
+            s = '<td style="color: green;">' + label_dict[prediction[5]] + '</td><td style="color: green;">' + label_dict[prediction[6]] +'</td>';
+        } else {
+            s = '<td style="color: red;">' + label_dict[prediction[5]] + '</td><td style="color: red;">' + label_dict[prediction[6]] +'</td>';
+        }
+        en_hate_speech_strings.push(s);
+        s= '</tr>';
         en_hate_speech_strings.push(s);
     }
     var s = '</table>';
